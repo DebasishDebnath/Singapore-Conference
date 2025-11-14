9a91faf76c22e3bb699435fa1bbda1a229c2281e
Debasish Debnath <ddebnath241@gmail.com>
2025-11-14 15:03:48 +0530

    Refactor OrganizingCommittee component: standardize function name and formatting

--- src/pages/Committee/organizingCommittee.jsx
+++ src/pages/Committee/organizingCommittee.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
-function organizingCommittee() {
-  return (
-    <div>
-      
-    </div>
-  )
+function OrganizingCommittee() {
+  return <div></div>;
 }
 
-export default organizingCommittee
+export default OrganizingCommittee;
