5dc2fe26adcb98374e876ad3b511d016f463da79
Debasish Debnath <ddebnath241@gmail.com>
2026-04-01 23:20:47 +0530

    Fix typo in submission deadline date in Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import SidebarCard from './SidebarCard'
 
 const content1=[
   {Date: 'April 10th, 2026', Detail: 'Full Paper Submission Deadline'},
-  {Date: 'May 1th, 2026', Detail: 'Notification of Acceptance'},
+  {Date: 'May 1st, 2026', Detail: 'Notification of Acceptance'},
   // {Date: 'May 15th - 22nd, 2026', Detail: 'Early Bird Registration'},
   {Date: 'May 10th, 2026', Detail: 'Camera Ready Paper Submission'},
   {Date: 'May 15th, 2026', Detail: 'Author Registration Deadline'},
